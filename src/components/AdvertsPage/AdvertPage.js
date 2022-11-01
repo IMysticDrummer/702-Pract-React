1c29d02c58752a5720ff7ec2bd0dfa63005c6552
Iván García Rodríguez <ivan.garcia.rodriguez@hotmail.es>
2022-11-01 16:54:09 +0100

    Get data to AdvertPage done

--- src/components/AdvertsPage/AdvertPage.js
+++ src/components/AdvertsPage/AdvertPage.js
@@ -1,7 +1,53 @@
-import Layout from '../Layout/Layout.js';
+import { Fragment, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Layout from "../Layout/Layout.js";
+import { getAdById } from "./service.js";
 
-const AdvertPage = ({subTitle}) => {
-  return <Layout subTitle={subTitle}><span>Por preparar</span></Layout>;
+const AdvertPage = ({ subTitle, isLogged, onLogout }) => {
+  const [ advertisement, setAdvertisement ] = useState({});
+  const { id } = useParams();
+  
+
+  useEffect(() => {
+    const getAd = async (id) => {
+      let advert;
+      try {
+        advert = await getAdById(id);
+        setAdvertisement(advert);
+      } catch (error) {
+        setAdvertisement({msg: 'Error retrieving data', error: error});
+      }
+    };
+    getAd(id);
+  }, [id]);
+
+  return (
+    <Layout
+      subTitle={subTitle}
+      isLogged={isLogged}
+      onLogout={onLogout}
+    >
+      <Fragment>
+        {!advertisement.error ? 
+          (
+            <Fragment>
+              <p>Id: {advertisement.id}</p>
+              <p>Created: {advertisement.createdAt}</p>
+              <p>Product: {advertisement.name}</p>
+              <p>Price: {advertisement.price}</p>
+              <p>{advertisement.sale ? "Sell" : "Buy"}</p>
+              <p>Photo: {advertisement.photo}</p>
+            </Fragment>
+          ) : (
+            <Fragment>
+              <p>{advertisement.msg}</p>
+              <p>{advertisement.error}</p>
+            </Fragment>
+          )
+        }
+      </Fragment>
+    </Layout>
+  );
 };
 
 export default AdvertPage;
--- src/components/AdvertsPage/service.js
+++ src/components/AdvertsPage/service.js
@@ -1,8 +1,13 @@
-'use strict';
-import client from '../../api/client.js';
+import client from "../../api/client.js";
 
-const advertisementsUrl = '/v1/adverts';
+const advertisementsUrl = "/v1/adverts";
+
 export const getAdvertisements = () => {
   const url = advertisementsUrl;
+  return client.get(url);
+};
+
+export const getAdById = (id) => {
+  const url = advertisementsUrl+'/'+id;
   return client.get(url);
 };
