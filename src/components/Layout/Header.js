d0e6660e50cd5724660410157b2ad6000c91864f
Iván García Rodríguez <ivan.garcia.rodriguez@hotmail.es>
2022-11-12 04:28:13 +0100

    Logout confirmation done

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,10 +1,27 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useLogin } from '../auth/context';
 import { Button } from '../common/Button';
+import ConfirmElement from '../common/ConfirmElement';
 import './Header.css';
 
 const Header = ({ title }) => {
   const { isLogged, setLogout: onLogout } = useLogin();
+  const [logoutRequired, setLogoutRequired] = useState(false);
+
+  const handleLogout = () => {
+    setLogoutRequired(true);
+  };
+
+  const doingLogout = (response) => {
+    if (response) {
+      setLogoutRequired(false);
+      onLogout();
+    } else {
+      setLogoutRequired(false);
+    }
+  };
+
   return (
     <header>
       <h1>{title || 'NODEPOP. Your second-hand sell/buy web'}</h1>
@@ -27,7 +44,13 @@ const Header = ({ title }) => {
             Advertisements list
           </NavLink>
         )}
-        {isLogged && <Button onClick={onLogout}>Log Out</Button>}
+        {isLogged && <Button onClick={handleLogout}>Log Out</Button>}
+        {logoutRequired && (
+          <ConfirmElement
+            message='Really are you going to quit?'
+            response={doingLogout}
+          />
+        )}
       </nav>
     </header>
   );
